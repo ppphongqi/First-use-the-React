c234abb09003c7112a16297022b4a3987ca7666c
ppphongqi <2503389490@qq.com>
2019-09-18 21:00:32 +0800

    xiaojiejie

---
+++ src/Xiaojiejie.js
@@ -0,0 +1,58 @@
+import React,{Component} from 'react';
+import './style.css'
+import XiaojiejieItem from './xiaojiejieItem'
+
+class Xiaojiejie extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            inputValue:'',
+            list : ['基础按摩','精油推背']
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <div>
+                    < input className={'input'} value = { this.state.inputValue }  onChange={this.inputChange.bind(this)} />
+                    <button onClick={this.addList.bind(this)}>增加服务</button>
+                </div>
+
+                <ul>
+                    {
+                        this.state.list.map((item,index)=>{
+                            return (
+                                <div>
+                                    <XiaojiejieItem/>
+                                </div>
+                            )
+                        })
+                    }
+
+                </ul>
+            </div>
+        )
+    }
+    inputChange(message){
+        this.setState({
+            inputValue: message.target.value
+        })
+    }
+    addList(){
+        this.setState({
+            list:[...this.state.list , this.state.inputValue],
+            inputValue : ""
+        })
+    }
+    deleteItem(index){
+        let list = this.state.list;
+        list.splice(index,1);
+        this.setState({
+            list : list
+        })
+    }
+
+}
+
+export default Xiaojiejie
---
+++ src/xiaojiejieItem.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+class xiaojiejieItem extends Component{
+    render(){
+        return (
+            <li>小姐姐</li>
+        )
+    }
+}
+export default xiaojiejieItem
