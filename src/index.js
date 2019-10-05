240fa001e5c7bb7b6a2d5db13df58ca7dfb82a8b
ppphongqi <2503389490@qq.com>
2019-10-05 23:47:39 +0800

    combineReducer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App'
 import TodoApp from './TodoApp'
+import {Provider} from 'react-redux';
+import store from './store'
+
 
 
 ReactDOM.render(
